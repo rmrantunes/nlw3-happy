be26808ca4ee0214722f329fd76f20957f43b467
Rafael Antunes <rafaelmessias710@gmail.com>
2020-10-15 13:39:53 -0400

    Before pages.js

--- src/server.js
+++ src/server.js
@@ -7,8 +7,14 @@ const server = express(); // o retorno da função é um objeto, com get, listen
 // "/" = index.html
 server
   .use(express.static("public")) // faz com que as pasta estatica "public" funcione
+  // configurar template engine Handlebars.js
+  // todos os arquivos que forem .html devem ser trocados por .hbs
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "hbs")
   .get("/", (req, res) => {
-    return res.sendFile(path.join(__dirname, "views", "index.html")); // A resposta é enviada para o cliente (ela vai dentro de res.sendFile(aqui))
+    // return res.sendFile(path.join(__dirname, "views", "index.html")); // A resposta é enviada para o cliente (ela vai dentro de res.sendFile(aqui))
+    // Com a template engine isso aqui ^^^ é feito assim:
+    return res.render("index");
   });
 
 // ligar o servidor
