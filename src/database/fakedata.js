8db87c13ced8b502d65f61a473763fc275e091c3
Rafael Antunes <rafaelmessias710@gmail.com>
2020-10-15 15:00:46 -0400

    fakedata for tests

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-27.2269864",
+    lng: "-49.6468318",
+    name: "Lar das Meninas",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1576024267263-70f1caffd6fe?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1595295413110-3304e36b564f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h às 18h",
+    open_on_weekends: "1",
+  },
+  {
+    id: 2,
+    lat: "-27.2269864",
+    lng: "-49.6568318",
+    name: "Casa dos Muleques",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1576024267263-70f1caffd6fe?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+      "https://images.unsplash.com/photo-1595295413110-3304e36b564f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h às 18h",
+    open_on_weekends: "0",
+  },
+];
