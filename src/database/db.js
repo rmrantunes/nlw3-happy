2b71a95792ba0cbabe4aac26a2fcef5bb29e43c9
Rafael Antunes <rafaelmessias710@gmail.com>
2020-10-16 11:06:00 -0400

    database script

---
+++ src/database/db.js
@@ -0,0 +1,21 @@
+// vai conter o
+const Database = require("sqlite-async");
+
+Database.open(__dirname + "/database.sqlite").then(execute);
+// criar um arquivo sqlite
+function execute(db) {
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS orphanages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      lat TEXT,
+      lng TEXT,
+      name TEXT,
+      about TEXT,
+      whatsapp TEXT,
+      images TEXT,
+      instructions TEXT,
+      opening_hours TEXT,
+      open_on_weekends TEXT
+    );
+  `);
+}
