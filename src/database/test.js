e679b1cd4e43bdb2d1925f09c9b071c260a15440
Rafael Antunes <rafaelmessias710@gmail.com>
2020-10-17 13:49:51 -0400

    Day 5 completed

--- src/database/test.js
+++ src/database/test.js
@@ -4,9 +4,9 @@ Database.then(async (db) => {
   // inserir dados na tabela
   await saveOrphanage(db, {
     lat: "-27.2269864",
-    lng: "-49.6768318",
-    name: "Casa da Paz",
-    description:
+    lng: "-49.6568318",
+    name: "lar das Meninos",
+    about :
       "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
     whatsapp: "464651",
     images: [
@@ -15,17 +15,17 @@ Database.then(async (db) => {
     ].toString(),
     instructions:
       "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-    opening_hours: "Horário de visitas das 7h às 15h",
+    opening_hours: "Horário de visitas das 8h às 18h",
     open_on_weekends: "0",
   });
   // consultar dados na tabela
   const selectedOrphanages = await db.all("SELECT * FROM orphanages");
   console.log(selectedOrphanages);
-  // consultar somente 1 orfanato pelo id;
-  const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"');
-  console.log(orphanage);
+  // // consultar somente 1 orfanato pelo id;
+  // const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"');
+  // console.log(orphanage);
 
-  // deletar dados da tabela
-  console.log(await db.run("DELETE FROM orphanages WHERE id = '5'"));
-  console.log(await db.run("DELETE FROM orphanages WHERE id = '6'"));
+  // // deletar dados da tabela
+  // console.log(await db.run("DELETE FROM orphanages WHERE id = '5'"));
+  // console.log(await db.run("DELETE FROM orphanages WHERE id = '6'"));
 });
