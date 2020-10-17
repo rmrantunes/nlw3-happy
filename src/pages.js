2aa907baf6a7f114e573b3e145c94a673d0260f0
Rafael Antunes <rafaelmessias710@gmail.com>
2020-10-17 13:45:16 -0400

    getting info from form and adding to database, and then showing all the active orphanages

--- src/pages.js
+++ src/pages.js
@@ -1,22 +1,79 @@
 // Este arquivo ficará responsável por conter as informações das páginas
 // que setá enviada pelo res.render()
 // principalmente por causa da template engine
-
-// importar dados do banco fakedata
-const orphanages = require("./database/fakedata.js");
+const Database = require("./database/db");
+const saveOrphanage = require("./database/saveOrphanage");
 
 // exportar para o server.js
 module.exports = {
   index(req, res) {
     return res.render("index");
   },
-  orphanage(req, res) {
-    return res.render("orphanage");
+  async orphanage(req, res) {
+    const id = req.query.id;
+    try {
+      const db = await Database;
+      const results = await db.all(
+        `SELECT * FROM orphanages WHERE id = "${id}"`,
+      );
+      const orphanage = results[0];
+
+      orphanage.images = orphanage.images.split(",");
+      orphanage.firstImage = orphanage.images[0];
+
+      orphanage.open_on_weekends =
+        orphanage.open_on_weekends == "0" ? false : true;
+
+      return res.render("orphanage", { orphanage });
+    } catch (error) {
+      console.log(error);
+      return res.send("Erro no banco de dados");
+    }
   },
-  orphanages(req, res) {
-    return res.render("orphanages", { orphanages });
+  async orphanages(req, res) {
+    // colocar o orphanage pelo banco de dados
+    try {
+      const db = await Database;
+      const orphanages = await db.all("SELECT * FROM orphanages");
+      return res.render("orphanages", { orphanages });
+    } catch (error) {
+      console.log(error);
+      return res.send("Erro no banco de dados");
+    }
   },
   createOrphanage(req, res) {
     return res.render("create-orphanage");
+  },
+  // POST formulatrio req.body são as informações que estão nos values dos inputs
+  async saveOrphanage(req, res) {
+    console.log(req.body);
+    const fields = req.body;
+    // validar se todos os campos estão preenchidos
+    if (Object.values(fields).includes("")) {
+      return res.send("todos os campos devem ser preencidos");
+    }
+
+    try {
+      // salvar um orfanato
+      const db = await Database;
+      await (db,
+      {
+        lat: fields.lat,
+        lng: fields.lng,
+        name: fields.name,
+        about: fields.about,
+        whatsapp: fields.whatsapp,
+        images: fields.images.toString(),
+        instructions: fields.instructions,
+        opening_hours: fields.opening_hours,
+        open_on_weekends: fields.open_on_weekends,
+      });
+
+      // redirecionamento
+      return res.redirect("/orphanages");
+    } catch (error) {
+      console.log(error);
+      return res.send("Erro no banco de dados");
+    }
   },
 };
